87114e4a65e273e0293eb1a68b77d654f5bf966d
Jose Daniel Vegas Martin <jvegmart@emeal.nttdata.com>
2024-05-04 13:13:20 +0200

    [MOD|ADD] Vista Usuario | Añadida la vista de actualizar usuario

--- FRONT-END/proyectotrivial/src/views/usuarios/AdminsView.vue
+++ FRONT-END/proyectotrivial/src/views/usuarios/AdminsView.vue
@@ -1,75 +1,85 @@
 <template>
-    <div class="container">
-    <div class="card">
-        <div class="card-header">
-            <h4>Usuarios 
-                <router-link to="/admin/crearUsuario" class="btn btn-primary float-end">Añadir Usuario</router-link></h4>    
-        </div> 
-        <div class="card-body">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                    <th>Id</th>
-                    <th>Nombre</th>
-                    <th>Email</th>
-                    <th>Imagen</th>
-                    <th>Cantidad</th>
-                    <th>Activo</th>
-                    </tr>
-                </thead>
-                <tbody v-if="this.usuario.length > 0">
-                    <tr v-for="(usuario, index) in this.usuarios" :key="index">
-                        <td>usuario.id</td>
-                        <td>usuario.name</td>
-                        <td>usuario.email</td>
-                        <td>usuario.imagen</td>
-                        <td>usuario.cantidad</td>
-                        <td>usuario.activo</td>
-                        <td>
-                            <router-link to="/" class="btn btn-success">
-                                Editar Usuario</router-link>
-                            <button type="button" class="btn btn-danger">Borrar Usuario</button>
-                        </td>
-                    </tr>
-                </tbody>
-                <tbody v-else>
-                    <tr>
-                        <td colspan="7">No hay usuarios</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>      
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="d-flex justify-content-between align-items-center">
+                    <span>Usuarios</span>
+                    <router-link to="/admin/crearUser" class="btn btn-primary">Añadir Usuario</router-link>
+                </h4>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Nombre</th>
+                            <th>Email</th>
+                            <th>Imagen</th>
+                            <th>Cantidad</th>
+                            <th>Activo</th>
+                        </tr>
+                    </thead>
+                    <tbody v-if="usuarios.length > 0">
+                        <tr v-for="(usuario, index) in usuarios" :key="index">
+                            <td>{{ usuario.id }}</td>
+                            <td>{{ usuario.name }}</td>
+                            <td>{{ usuario.email }}</td>
+                            <td>{{ usuario.imagen }}</td>
+                            <td>{{ usuario.cantidad }}</td>
+                            <td>{{ usuario.activo }}</td>
+                            <td>
+                                <router-link to="/" class="btn btn-success">
+                                    Editar Usuario
+                                </router-link>
+                                <button type="button" class="btn btn-danger">Borrar Usuario</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tbody v-else>
+                        <tr>
+                            <td colspan="7">No hay usuarios</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>      
+        </div>
     </div>
-</div>
 </template>
+
 <script>
 import axios from 'axios'
-export default{
+
+export default {
     name: 'admin',
-    data(){
-        return{
-        usuarios: []
-    }
-},
-mounted(){
-    this.getUsuarios();
-},
-method:{
-    getUsuarios(){
-        axios.get('http://localhost:8080/trivial/v1/users')
+    data() {
+        return {
+            usuarios: []
+        }
+    },
+    mounted() {
+        this.getUsuarios();
+    },
+    methods: {
+        getUsuarios() {
+            axios.get('http://localhost:8080/trivial/v1/users')
                 .then(response => {
                     console.log(response)
-                    this.users = response.data;
+                    this.usuarios = response.data;
                 })
                 .catch(error => {
                     console.error('Hubo un problema con la solicitud:', error);
-                    
                 });
         },
     }
 }
-
 </script>
-<style>
 
+<style scoped>
+.card-header h4 {
+    margin-bottom: 0;
+}
+.card-header .btn {
+    margin-left: auto;
+    margin-top: 10px; /* Ajusta la separación del botón desde la parte superior */
+}
 </style>
