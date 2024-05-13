a67f5854686b7c4ef1db5152f9bbb1ac0d3af48f
Álvaro <lvaro>
2024-05-13 10:55:40 +0200

    Limpieza de Codigo en Backend

--- BACK-END/ProyectoTrivial/src/main/java/com/proyecto/configuracion/CorsConfig.java
+++ BACK-END/ProyectoTrivial/src/main/java/com/proyecto/configuracion/CorsConfig.java
@@ -2,7 +2,6 @@ package com.proyecto.configuracion;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
@@ -11,43 +10,13 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-//public class CorsConfig implements WebMvcConfigurer {
-//
-//	@Override
-//	public void addCorsMappings(CorsRegistry registry) {
-////		registry.addMapping("/trivial/v1/categoria").allowedOrigins("http://localhost:8081").allowedMethods("GET");
-////		registry.addMapping("/trivial/v1/ranking").allowedOrigins("http://localhost:8081").allowedMethods("GET");
-////		registry.addMapping("/trivial/v1/roles").allowedOrigins("http://localhost:8081").allowedMethods("GET");
-//		registry.addMapping("/trivial/v1/users").allowedOrigins("http://localhost:8081").allowedMethods("GET", "POST",
-//				"PUT", "DELETE");
-////		registry.addMapping("/trivial/v1/**").allowedOrigins("http://localhost:8081")
-////				.allowedMethods("GET", "POST", "PUT", "DELETE").allowCredentials(true);
-//	}
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/trivial/v1/users/**")
-//            .allowedOrigins("http://localhost:8081")
-//            .allowedMethods("GET", "POST", "PUT", "DELETE")
-//            .allowedHeaders("*")
-//            .allowCredentials(true)
-//            .maxAge(3600);
-//    }
-//	@Override
-//	public void addCorsMappings(CorsRegistry registry) {
-//		registry.addMapping("/trivial/v1/users/**").allowedOrigins("http://localhost:8081")
-//				.allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("Content-Type", "Authorization")
-//				.maxAge(3600);
-//	}
+
 public class CorsConfig implements Filter {
 
 	@Override
@@ -65,6 +34,5 @@ public class CorsConfig implements Filter {
 		} else {
 			chain.doFilter(req, res);
 		}
-
 	}
 }
--- BACK-END/ProyectoTrivial/src/main/java/com/proyecto/trivial/controllers/rest/UsersRestController.java
+++ BACK-END/ProyectoTrivial/src/main/java/com/proyecto/trivial/controllers/rest/UsersRestController.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,8 +22,6 @@ import org.springframework.web.multipart.MultipartFile;
 import com.proyecto.trivial.dtos.UsersDTO;
 import com.proyecto.trivial.entities.UserEntity;
 import com.proyecto.trivial.repositories.IUsersRepository;
-
-import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:8081")
